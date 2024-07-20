2d3ab7ae0231a728de12664250cb5c807cac7757
sontung1010 <sontungcodedao@gmail.com>
2024-07-19 22:35:32 -0700

    completed 28

--- 28.find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28.find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -5,10 +5,26 @@
  */
 
 // @lc code=start
+#include <iostream>
+#include <string>
+
+using namespace std;
+
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        
+        if (haystack.size() < needle.size()) {
+            return -1;
+        }
+
+        for (int i = 0; i < haystack.size() - needle.size() + 1; i++) {
+            string substr = haystack.substr(i, needle.size());
+            if (substr == needle) {
+                return i;
+            }
+        }
+
+        return -1; 
     }
 };
 // @lc code=end
